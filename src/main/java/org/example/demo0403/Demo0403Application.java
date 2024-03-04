9ce7df8614727e55c13b1ac89d0d47701e6e57d1
EugenyB <eberkunsky@gmail.com>
2024-03-04 19:40:09 +0200

    Initial commit

---
+++ src/main/java/org/example/demo0403/Demo0403Application.java
@@ -0,0 +1,13 @@
+package org.example.demo0403;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo0403Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo0403Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/demo0403/Demo0403ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.demo0403;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo0403ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
